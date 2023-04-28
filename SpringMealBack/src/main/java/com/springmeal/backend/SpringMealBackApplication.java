4d818df847a2edfc8035d8ecd4248b326e19bbd5
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-04-28 08:57:08 +0200

    Init Commit

---
+++ SpringMealBack/src/main/java/com/springmeal/backend/SpringMealBackApplication.java
@@ -0,0 +1,13 @@
+package com.springmeal.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMealBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMealBackApplication.class, args);
+	}
+
+}
---
+++ SpringMealBack/src/test/java/com/springmeal/backend/SpringMealBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmeal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMealBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
