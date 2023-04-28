9172f6cf8068a11261c10539f63320e84ebe63aa
Palmira Romia <kguinera@hotmail.com>
2023-04-28 11:02:22 +0200

    dto Order created

--- SpringMealBack/src/main/java/com/springmeal/backend/dto/Order.java
+++ SpringMealBack/src/main/java/com/springmeal/backend/dto/Order.java
@@ -3,7 +3,14 @@
  */
 package com.springmeal.backend.dto;
 
+import java.sql.Date;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 /**
@@ -11,7 +18,96 @@ import jakarta.persistence.Table;
  *
  */
 @Entity
-@Table(name="orders")
+@Table(name = "orders")
 public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private Date date;
+
+	@ManyToOne
+	@JoinColumn(name = "id_slot")
+	private Slot slot;
+
+	@ManyToOne
+	@JoinColumn(name = "id_user")
+	private User user;
+
+	// Constructors
+
+	public Order() {
+
+	}
+
+	/**
+	 * @param id
+	 * @param date
+	 * @param slot
+	 * @param user
+	 */
+	public Order(int id, Date date, Slot slot, User user) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.slot = slot;
+		this.user = user;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	/**
+	 * @return the slot
+	 */
+	public Slot getSlot() {
+		return slot;
+	}
+
+	/**
+	 * @param slot the slot to set
+	 */
+	public void setSlot(Slot slot) {
+		this.slot = slot;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 }
