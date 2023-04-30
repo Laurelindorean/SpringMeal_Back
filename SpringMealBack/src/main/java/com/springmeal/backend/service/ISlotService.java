8276f1885dc526e07be0801b603bbd8a9f943428
Palmira Romia <kguinera@hotmail.com>
2023-04-30 02:21:06 +0200

    IService complete

--- SpringMealBack/src/main/java/com/springmeal/backend/service/ISlotService.java
+++ SpringMealBack/src/main/java/com/springmeal/backend/service/ISlotService.java
@@ -3,10 +3,25 @@
  */
 package com.springmeal.backend.service;
 
+import java.util.List;
+
+import com.springmeal.backend.dto.Slot;
+
 /**
  * @author Palmira
  *
  */
 public interface ISlotService {
+
+	// CRUD Methods
+	public List<Slot> listSlot();
+
+	public Slot saveSlot(Slot slot);
+
+	public Slot slotByID(int id);
+
+	public Slot updateSlot(Slot slot);
+
+	public void deleteSlot(int id);
 
 }
