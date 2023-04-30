1f830c13886a7949959e2249732c3248af35d751
Palmira Romia <kguinera@hotmail.com>
2023-04-30 02:28:46 +0200

    ServiceImpl complete

--- SpringMealBack/src/main/java/com/springmeal/backend/service/OrderServiceImpl.java
+++ SpringMealBack/src/main/java/com/springmeal/backend/service/OrderServiceImpl.java
@@ -3,10 +3,45 @@
  */
 package com.springmeal.backend.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springmeal.backend.dao.IOrderDAO;
+import com.springmeal.backend.dto.Order;
+
 /**
  * @author Palmira
  *
  */
-public class OrderServiceImpl {
+public class OrderServiceImpl implements IOrderService {
+
+	@Autowired
+	IOrderDAO iOrderDAO;
+
+	@Override
+	public List<Order> listOrder() {
+		return iOrderDAO.findAll();
+	}
+
+	@Override
+	public Order saveOrder(Order order) {
+		return iOrderDAO.save(order);
+	}
+
+	@Override
+	public Order orderById(int id) {
+		return iOrderDAO.findById(id).get();
+	}
+
+	@Override
+	public Order updateOrder(Order order) {
+		return iOrderDAO.save(order);
+	}
+
+	@Override
+	public void deleteOrder(int id) {
+		iOrderDAO.deleteById(id);
+	}
 
 }
--- SpringMealBack/src/main/java/com/springmeal/backend/service/SlotServiceImpl.java
+++ SpringMealBack/src/main/java/com/springmeal/backend/service/SlotServiceImpl.java
@@ -3,10 +3,45 @@
  */
 package com.springmeal.backend.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springmeal.backend.dao.ISlotDAO;
+import com.springmeal.backend.dto.Slot;
+
 /**
  * @author Palmira
  *
  */
-public class SlotServiceImpl {
+public class SlotServiceImpl implements ISlotService {
+
+	@Autowired
+	ISlotDAO iSlotDAO;
+
+	@Override
+	public List<Slot> listSlot() {
+		return iSlotDAO.findAll();
+	}
+
+	@Override
+	public Slot saveSlot(Slot slot) {
+		return iSlotDAO.save(slot);
+	}
+
+	@Override
+	public Slot slotByID(int id) {
+		return iSlotDAO.findById(id).get();
+	}
+
+	@Override
+	public Slot updateSlot(Slot slot) {
+		return iSlotDAO.save(slot);
+	}
+
+	@Override
+	public void deleteSlot(int id) {
+		iSlotDAO.deleteById(id);
+	}
 
 }
