303e8fcb111ea773bbade3ada4406fddc9163dcc
Joan <joan.lapeyra@estudiantat.upc.edu>
2023-05-19 14:19:00 +0200

    Revert "allow cors mappings (needed by front)"
    This reverts commit 22b71fbe84cff1478cf44edb5f836a6e8dda585b.

--- SpringMealBack/src/main/java/com/springmeal/backend/security/WebSecurityConfig.java
+++ SpringMealBack/src/main/java/com/springmeal/backend/security/WebSecurityConfig.java
@@ -16,20 +16,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.springmeal.backend.security.jwt.AuthEntryPointJwt;
 import com.springmeal.backend.security.jwt.JwtAuthenticationFilter;
 import com.springmeal.backend.security.service.UserDetailsServiceImpl;
 
 /**
- * @author Palmira, Joan
+ * @author Palmira
  *
  */
 @Configuration
 @EnableMethodSecurity
-public class WebSecurityConfig  implements WebMvcConfigurer {
+public class WebSecurityConfig {
 
 	@Autowired
 	private UserDetailsServiceImpl userDetailsService;
@@ -78,15 +76,5 @@ public class WebSecurityConfig  implements WebMvcConfigurer {
 		return http.build();
 
 	}
-
-	@Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-    }
-
 
 }
